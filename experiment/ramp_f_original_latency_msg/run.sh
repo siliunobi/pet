21e610d944e51e2f026e2e99bd3b315e7be10dfe
Atul Sandur <nobi@node0.nobi-qv62730.confluence.emulab.net>
2019-12-28 12:37:39 -0700

    scripts

---
+++ experiment/ramp_f_original_latency_msg/run.sh
@@ -0,0 +1,38 @@
+host=`hostname -s`
+test_file=test.maude
+
+rm result.out
+for dist in `cat dist`
+do
+    for load in `cat load`
+    do
+        for key in `cat keys`
+        do
+	    conf="$load-$key-$dist-2-2"
+            echo $conf
+	    python gen.py test.maude.temp test.maude $load 200 $key  $dist 2 2
+	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f avglatency.quatex -a 0.05 > ${host}-$conf.out 2>&1
+	    result=`python result.py ${host}-$conf.out`
+	    echo $conf $result
+	    echo $conf $result >> result.out
+        done
+    done
+done
+
+for dist in `cat dist`
+do
+    for load in `cat load`
+    do
+        for key in `cat keys`
+        do
+	    conf="$load-$key-$dist-8-8"
+            echo $conf
+	    python gen.py test.maude.temp test.maude $load 200 $key  $dist 8 8
+	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f avglatency.quatex -a 0.05 > ${host}-$conf.out 2>&1
+	    result=`python result.py ${host}-$conf.out`
+	    echo $conf $result
+	    echo $conf $result >> result.out
+        done
+    done
+done
+
