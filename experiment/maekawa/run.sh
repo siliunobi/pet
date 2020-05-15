02811f65972559d415f856e1d32f942fd6d66351
Nobi <uiucnobi@gmail.com>
2020-05-15 17:12:59 +0200

    updated token ring with all joining & updated maekawa with avgWait

--- experiment/maekawa/run.sh
+++ experiment/maekawa/run.sh
@@ -4,11 +4,11 @@ test_file=test.maude
 
 for node in `cat node`
 do
-	    conf="$node"
+	    conf="$node-avgWait"
         echo $conf
 	    python gen.py test.maude.temp test.maude $node
 		echo `date`
-	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f deadlock.quatex -a 0.05 > ${host}-$conf.out 2>&1
+	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f avgWait.quatex -a 0.05 > ${host}-$conf.out 2>&1
 	    result=`python result.py ${host}-$conf.out`
 	    echo $conf $result
 	    echo $conf $result >> result.out
--- experiment/mutex/run.sh
+++ experiment/mutex/run.sh
@@ -6,36 +6,6 @@ for node in `cat node`
 do
     for bias in `cat bias`
     do
-	    conf="$node-$bias-median"
-        echo $conf
-	    python gen.py test.maude.temp test.maude $node $bias
-		echo `date`
-	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f median.quatex -a 0.05 > ${host}-$conf.out 2>&1
-	    result=`python result.py ${host}-$conf.out`
-	    echo $conf $result
-	    echo $conf $result >> result.out
-    done
-done
-
-for node in `cat node`
-do
-    for bias in `cat bias`
-    do
-	    conf="$node-$bias-longestWait"
-        echo $conf
-	    python gen.py test.maude.temp test.maude $node $bias
-		echo `date`
-	    java -jar ~/pvesta/pvesta-client.jar -l ~/pvesta/serverlist1 -m ${test_file} -f longestWait.quatex -a 0.05 > ${host}-$conf.out 2>&1
-	    result=`python result.py ${host}-$conf.out`
-	    echo $conf $result
-	    echo $conf $result >> result.out
-    done
-done
-
-for node in `cat node`
-do
-    for bias in `cat bias`
-    do
 	    conf="$node-$bias-avgWait"
         echo $conf
 	    python gen.py test.maude.temp test.maude $node $bias
